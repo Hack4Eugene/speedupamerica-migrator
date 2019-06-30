0f4fe8e76b06399732e9c5966e2f7e6959253522
Ryan Olds <ryanrolds@gmail.com>
2019-06-30 16:22:34 -0700

    Expanded the tables to include breakdowns

--- db/migrate/20190628164100_add_stats_cache_table.rb
+++ db/migrate/20190628164100_add_stats_cache_table.rb
@@ -1,28 +1,68 @@
 class AddStatsCacheTable < ActiveRecord::Migration
-    def change
-      create_table :stats_cache, id: false do |t|
-        t.string :boundary_type, null:false
-        t.string :boundary_id, null:false
-        t.string :date_type, null:false
-        t.date :date_value, null:false
+  def change
+    create_table :stats_caches, id: false do |t|
+      t.string :stat_type, null:false
+      t.string :stat_id, null:false
+      t.string :date_type, null:false
+      t.date :date_value, null:false       
 
-        t.float :all_avg_upload, null:false
-        t.float :all_median_upload, null:false
-        t.integer :all_count_upload, null:false
-        t.float :all_avg_download, null:false
-        t.float :all_median_download, null:false
-        t.integer :all_count_download, null:false
-        t.float :sua_avg_upload, null:false
-        t.float :sua_median_upload, null:false
-        t.integer :sua_count_upload, null:false
-        t.float :sua_avg_download, null:false
-        t.float :sua_median_download, null:false
-        t.integer :sua_count_download, null:false
-  
-        t.timestamps null: false
-      end
+      t.float :download_avg, null:false
+      t.float :download_median, null:false
+      t.float :download_max, null:false
+      t.integer :download_count, null:false
 
-      execute "ALTER TABLE stats_cache ADD PRIMARY KEY (boundary_type, boundary_id, date_type, date_value);"
+      t.float :download_sua_avg, null:false
+      t.float :download_sua_median, null:false
+      t.float :download_sua_max, null:false
+      t.integer :download_sua_count, null:false
+
+      t.integer :download_0_5, null:false
+      t.integer :download_6_10, null:false
+      t.integer :download_11_20, null:false
+      t.integer :download_21_40, null:false
+      t.integer :download_40_60, null:false
+      t.integer :download_61_80, null:false
+      t.integer :download_81_100, null:false
+      t.integer :download_101_250, null:false
+      t.integer :download_251_500, null:false
+      t.integer :download_500_1000, null:false
+      t.integer :download_1001, null:false
+
+      t.integer :download_less_than_5, null:false
+      t.integer :download_less_than_25, null:false
+      t.integer :download_faster_than_100, null:false
+      t.integer :download_faster_than_250, null:false
+
+      t.float :upload_avg, null:false
+      t.float :upload_median, null:false
+      t.float :upload_max, null:false
+      t.integer :upload_count, null:false
+      
+      t.float :upload_sua_avg, null:false
+      t.float :upload_sua_median, null:false
+      t.float :upload_sua_max, null:false
+      t.integer :upload_sua_count, null:false
+
+      t.integer :upload_0_5, null:false
+      t.integer :upload_6_10, null:false
+      t.integer :upload_11_20, null:false
+      t.integer :upload_21_40, null:false
+      t.integer :upload_40_60, null:false
+      t.integer :upload_61_80, null:false
+      t.integer :upload_81_100, null:false
+      t.integer :upload_101_250, null:false
+      t.integer :upload_251_500, null:false
+      t.integer :upload_500_1000, null:false
+      t.integer :upload_1001, null:false
+
+      t.integer :upload_less_than_5, null:false
+      t.integer :upload_less_than_25, null:false
+      t.integer :upload_faster_than_100, null:false
+      t.integer :upload_faster_than_250, null:false
+
+      t.timestamps null: false
     end
+
+    execute "ALTER TABLE stats_caches ADD PRIMARY KEY (stat_type, stat_id, date_type, date_value);"
   end
-  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -46,25 +46,59 @@ ActiveRecord::Schema.define(version: 20190723184600) do
     t.string   "short_name", limit: 255
   end
 
-  create_table "stats_cache", id: false, force: :cascade do |t|
-    t.string   "boundary_type",       limit: 255, null: false
-    t.string   "boundary_id",         limit: 255, null: false
-    t.string   "date_type",           limit: 255, null: false
-    t.date     "date_value",                      null: false
-    t.float    "all_avg_upload",      limit: 24,  null: false
-    t.float    "all_median_upload",   limit: 24,  null: false
-    t.integer  "all_count_upload",    limit: 4,   null: false
-    t.float    "all_avg_download",    limit: 24,  null: false
-    t.float    "all_median_download", limit: 24,  null: false
-    t.integer  "all_count_download",  limit: 4,   null: false
-    t.float    "sua_avg_upload",      limit: 24,  null: false
-    t.float    "sua_median_upload",   limit: 24,  null: false
-    t.integer  "sua_count_upload",    limit: 4,   null: false
-    t.float    "sua_avg_download",    limit: 24,  null: false
-    t.float    "sua_median_download", limit: 24,  null: false
-    t.integer  "sua_count_download",  limit: 4,   null: false
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+  create_table "stats_caches", id: false, force: :cascade do |t|
+    t.string   "stat_type",                limit: 255, null: false
+    t.string   "stat_id",                  limit: 255, null: false
+    t.string   "date_type",                limit: 255, null: false
+    t.date     "date_value",                           null: false
+    t.float    "download_avg",             limit: 24,  null: false
+    t.float    "download_median",          limit: 24,  null: false
+    t.float    "download_max",             limit: 24,  null: false
+    t.integer  "download_count",           limit: 4,   null: false
+    t.float    "download_sua_avg",         limit: 24,  null: false
+    t.float    "download_sua_median",      limit: 24,  null: false
+    t.float    "download_sua_max",         limit: 24,  null: false
+    t.integer  "download_sua_count",       limit: 4,   null: false
+    t.integer  "download_0_5",             limit: 4,   null: false
+    t.integer  "download_6_10",            limit: 4,   null: false
+    t.integer  "download_11_20",           limit: 4,   null: false
+    t.integer  "download_21_40",           limit: 4,   null: false
+    t.integer  "download_40_60",           limit: 4,   null: false
+    t.integer  "download_61_80",           limit: 4,   null: false
+    t.integer  "download_81_100",          limit: 4,   null: false
+    t.integer  "download_101_250",         limit: 4,   null: false
+    t.integer  "download_251_500",         limit: 4,   null: false
+    t.integer  "download_500_1000",        limit: 4,   null: false
+    t.integer  "download_1001",            limit: 4,   null: false
+    t.integer  "download_less_than_5",     limit: 4,   null: false
+    t.integer  "download_less_than_25",    limit: 4,   null: false
+    t.integer  "download_faster_than_100", limit: 4,   null: false
+    t.integer  "download_faster_than_250", limit: 4,   null: false
+    t.float    "upload_avg",               limit: 24,  null: false
+    t.float    "upload_median",            limit: 24,  null: false
+    t.float    "upload_max",               limit: 24,  null: false
+    t.integer  "upload_count",             limit: 4,   null: false
+    t.float    "upload_sua_avg",           limit: 24,  null: false
+    t.float    "upload_sua_median",        limit: 24,  null: false
+    t.float    "upload_sua_max",           limit: 24,  null: false
+    t.integer  "upload_sua_count",         limit: 4,   null: false
+    t.integer  "upload_0_5",               limit: 4,   null: false
+    t.integer  "upload_6_10",              limit: 4,   null: false
+    t.integer  "upload_11_20",             limit: 4,   null: false
+    t.integer  "upload_21_40",             limit: 4,   null: false
+    t.integer  "upload_40_60",             limit: 4,   null: false
+    t.integer  "upload_61_80",             limit: 4,   null: false
+    t.integer  "upload_81_100",            limit: 4,   null: false
+    t.integer  "upload_101_250",           limit: 4,   null: false
+    t.integer  "upload_251_500",           limit: 4,   null: false
+    t.integer  "upload_500_1000",          limit: 4,   null: false
+    t.integer  "upload_1001",              limit: 4,   null: false
+    t.integer  "upload_less_than_5",       limit: 4,   null: false
+    t.integer  "upload_less_than_25",      limit: 4,   null: false
+    t.integer  "upload_faster_than_100",   limit: 4,   null: false
+    t.integer  "upload_faster_than_250",   limit: 4,   null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
   end
 
   create_table "submissions", force: :cascade do |t|
@@ -104,6 +138,7 @@ ActiveRecord::Schema.define(version: 20190723184600) do
   end
 
   add_index "submissions", ["actual_down_speed"], name: "index_submissions_on_actual_down_speed", using: :btree
+  add_index "submissions", ["census_code"], name: "index_submissions_on_census_code", using: :btree
   add_index "submissions", ["census_status"], name: "index_submissions_on_census_status", using: :btree
   add_index "submissions", ["country_code", "region", "test_type"], name: "index_submissions_on_country_code_and_region_and_test_type", using: :btree
   add_index "submissions", ["provider", "test_date", "test_type"], name: "index_submissions_on_provider_and_test_date_and_test_type", using: :btree
