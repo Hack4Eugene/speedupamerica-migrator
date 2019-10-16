8cca45267003ef8d2b90d12a6e28139152809de8
Ryan Olds <ryanrolds@gmail.com>
2019-10-15 21:03:04 -0700

    Only enable Lane County blocks

--- db/migrate/20190925182200_enable_states.rb
+++ db/migrate/20190925182200_enable_states.rb
@@ -1,6 +1,8 @@
 class EnableStates < ActiveRecord::Migration
-  # Enable counties, tracts, and blocks in Oregon, Washington, Idaho
-  execute "UPDATE boundaries SET enabled = true WHERE boundary_type IN ('county', 'census_tract', 'census_block') AND boundary_id REGEXP '^(41|53|16)'"
+  # Enable counties, tracts in Oregon, Washington, Idaho
+  execute "UPDATE boundaries SET enabled = true WHERE boundary_type IN ('county', 'census_tract') AND boundary_id REGEXP '^(41|53|16)'"
+  # Enable blocks for Lane County Oregon
+  execute "UPDATE boundaries SET enabled = true WHERE boundary_type = 'census_block' AND boundary_id REGEXP '^41039'"
   # Washington ZIPs
   execute "UPDATE boundaries SET enabled = true WHERE boundary_type = 'zip_code' AND boundary_id REGEXP '^(980|981|982|983|984|985|986|987|988|989|990|991|992|993|994)'"
   # Oregon ZIPs
@@ -8,5 +10,3 @@ class EnableStates < ActiveRecord::Migration
   # Idaho ZIPs
   execute "UPDATE boundaries SET enabled = true WHERE boundary_type = 'zip_code' AND boundary_id REGEXP '^(832|833|834|835|836|837|838|839)'"
 end
-
-
